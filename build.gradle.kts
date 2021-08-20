f5bfa6415cdb0e274a1ed67cad47def547cdd229
Nep <spica@starry.blue>
2021-08-20 11:23:33 +0900

    Update build.gradle.kts

--- build.gradle.kts
+++ build.gradle.kts
@@ -2,22 +2,15 @@ plugins {
     kotlin("jvm") version "1.5.20"
     kotlin("plugin.serialization") version "1.5.20"
     id("com.github.johnrengelman.shadow") version "7.0.0"
-
-    id("org.jlleitschuh.gradle.ktlint") version "10.1.0"
-    id("com.adarshr.test-logger") version "2.1.1"
-    id("net.rdrei.android.buildtimetracker") version "0.11.0"
 }
 
 object Versions {
-    const val Penicillin = "6.2.0"
+    const val Penicillin = "6.2.1"
     const val TwitterText = "3.1.0"
-    const val Ktor = "1.6.0"
+    const val Ktor = "1.6.2"
 
-    const val KotlinLogging = "2.0.8"
+    const val KotlinLogging = "2.0.10"
     const val Logback = "1.2.3"
-    const val jansi = "2.3.3"
-
-    const val JUnit = "5.7.2"
 }
 
 object Libraries {
@@ -29,8 +22,6 @@ object Libraries {
     const val KotlinLogging = "io.github.microutils:kotlin-logging:${Versions.KotlinLogging}"
     const val LogbackCore = "ch.qos.logback:logback-core:${Versions.Logback}"
     const val LogbackClassic = "ch.qos.logback:logback-classic:${Versions.Logback}"
-    const val Jansi = "org.fusesource.jansi:jansi:${Versions.jansi}"
-    const val JUnitJupiter = "org.junit.jupiter:junit-jupiter:${Versions.JUnit}"
 
     val ExperimentalAnnotations = setOf(
         "kotlin.io.path.ExperimentalPathApi",
@@ -52,11 +43,6 @@ dependencies {
     implementation(Libraries.KotlinLogging)
     implementation(Libraries.LogbackCore)
     implementation(Libraries.LogbackClassic)
-    implementation(Libraries.Jansi)
-
-    testImplementation(kotlin("test"))
-    testImplementation(kotlin("test-junit5"))
-    testImplementation(Libraries.JUnitJupiter)
 }
 
 kotlin {
@@ -79,50 +65,6 @@ kotlin {
             languageSettings.useExperimentalAnnotation(it)
         }
     }
-}
-
-/*
- * Tests
- */
-
-ktlint {
-    verbose.set(true)
-    outputToConsole.set(true)
-    reporters {
-        reporter(org.jlleitschuh.gradle.ktlint.reporter.ReporterType.CHECKSTYLE)
-    }
-    ignoreFailures.set(true)
-}
-
-buildtimetracker {
-    reporters {
-        register("summary") {
-            options["ordered"] = "true"
-            options["barstyle"] = "ascii"
-            options["shortenTaskNames"] = "false"
-        }
-    }
-}
-
-tasks.withType<Test> {
-    useJUnitPlatform()
-
-    testLogging {
-        showStandardStreams = true
-        events("passed", "failed")
-    }
-
-    testlogger {
-        theme = com.adarshr.gradle.testlogger.theme.ThemeType.MOCHA_PARALLEL
-    }
-}
-
-task<JavaExec>("run") {
-    dependsOn("build")
-
-    group = "application"
-    main = "blue.starry.epgbird.MainKt"
-    classpath(configurations.runtimeClasspath, tasks.jar)
 }
 
 tasks.withType<com.github.jengelman.gradle.plugins.shadow.tasks.ShadowJar> {
